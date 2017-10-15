567ef857179841e1d04e3eb71b836b4cf8e00131
Stef <bonea_r@yahoo.com>
2017-10-15 10:25:07 -0600

    Changed the js file

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,19 @@
 $(function() {
 
+ $.ajax({
+   url:'https://www.codeschool.com/users/3839571.json',
+   dataType:'jsonp',
+   success:function(response){
+     var courses = response.courses.completed;
+     for(var index = 0; index < courses.length; index++){
+      $('#badges').append('<div><h3>' + courses[index].title + '</h3><img src="'+ courses[index].badge + '"></img><a href="'+ courses[index].url+'" target="_blank">See Course</a></div>');
+
+     }
+     $('#badges').find('div').addClass('course');
+     $('#badges').find('a').addClass('btn btn-primary' );
+  },
+   contentType:'appliction/json'
+ });
   // your code will go here
 
 });
